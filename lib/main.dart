440ad53991ad51c8e4e42e14013dfbb03aacb72b
Osheen Tikku <32721916+osheentikku@users.noreply.github.com>
2023-07-13 10:27:06 -0700

    adding google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/login_page.dart';
 
 void main() {
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tippy Toes Daycare',
-      theme: ThemeData(        
+      theme: ThemeData(  
+        fontFamily: GoogleFonts.inter().fontFamily, 
       ),
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
